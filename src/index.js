bedf031fa6b214e5e0fcdf33229c8695fcb22e43
Ariokhx <Vladdrob@gmail.com>
2021-03-16 01:17:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array == 0) return 0
+  else return Math.min.apply(0,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array == 0) return 0
+  else return Math.max.apply(0,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array == 0) return 0
+  else return array.reduce((a,b) =>(a+b))/array.length
 }
